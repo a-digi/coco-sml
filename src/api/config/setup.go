3f1a61a1a0f1e743dab0597585760563084b77df
andet tafa <andet.tafa@verso.de>
2025-12-30 23:49:42 +0100

    =

--- src/api/config/setup.go
+++ src/api/config/setup.go
@@ -7,17 +7,11 @@ import (
 
 // SetupApi initializes the API setup with a given binary.Server
 func SetupApi(server *binary.Server) {
-	log.Println("[SetupApi] Checking for database 'internal' ...")
 	_, err := server.FindDatabase("internal")
 	if err != nil {
-		log.Println("[SetupApi] Database 'internal' does not exist. Creating...")
 		errCreate := server.DatabaseCreate("internal", "Internal system database")
 		if errCreate != nil {
 			log.Printf("[SetupApi] Failed to create database 'internal': %v\n", errCreate)
-		} else {
-			log.Println("[SetupApi] Database 'internal' created successfully.")
 		}
-	} else {
-		log.Println("[SetupApi] Database 'internal' already exists.")
 	}
 }
